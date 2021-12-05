4e3a1b11958faa5463e82dbdbdbeee42a9af557b
m0uka <lukasekmoucka0@gmail.com>
2021-12-05 18:40:47 +0100

    fix: shipping address not required

--- ParadiseCommerce.Services.Ordering/Models/ShippingAddress.cs
+++ ParadiseCommerce.Services.Ordering/Models/ShippingAddress.cs
@@ -4,19 +4,14 @@ namespace ParadiseCommerce.Services.Ordering.Models
 {
     public class ShippingAddress
     {
-        [Required]
         public string State { get; set; }
         
-        [Required]
         public string StreetAddress { get; set; }
         
-        [Required]
         public string City { get; set; }
         
-        [Required]
         public string ZipCode { get; set; }
         
-        [Required]
         public string Country { get; set; }
     }
 }
